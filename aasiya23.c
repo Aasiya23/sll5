f882b2a554ddac19e9c7b51fda7a4d6aac383650
NandiniMuchandi1 <nandinimuchandi@gmail.com>
2024-12-07 22:27:21 +0530

    this is delete_end file

---
+++ aasiya23.c
@@ -0,0 +1,18 @@
+void delet_end()
+{
+NODE temp,prev;
+temp=first;
+if(temp->next==NULL)
+first=NULL;
+else
+{
+while(temp->next!=NULL)
+{
+prev=temp;
+temp=temp->next;
+}
+prev->next=NULL;
+}
+printf("\nthe deleted student usn is:
+%s",temp->usn);
+}
